e1ada3272e2cbebb0862beeb23dc4305842f85c8
vinyaa <kmm137@pitt.edu>
2014-06-17 19:12:20 -0400

    Added the rest of the document
    Not sure how I got that partial...

--- testBank.py
+++ testBank.py
@@ -10,7 +10,7 @@ class testBank(unittest.TestCase):
 
 
 	'''
-	Test to ensure the instantiated b is not null
+	Test to ensure the instantiated Bank b is not null
 	'''
 	def testInit(self):
 		self.assertIsNotNone(self.b)
@@ -25,7 +25,8 @@ class testBank(unittest.TestCase):
 
 	'''
 	Test to ensure an exception is raised if you try to get 
-	an account that doesn't exist
+	an account that doesn't exist.
+	Try to get an account while the bank has no accounts
 	'''
 	def testGetAccountFailure(self):
 		self.assertRaises(Exception, self.b.getAccount,2)
@@ -40,27 +41,67 @@ class testBank(unittest.TestCase):
 		self.assertIsInstance(self.b.getAccount(1),BankAccount)
 
 
+	'''
+	Test removeAccount to ensure that when the bank has no accounts, removing one
+	will not throw an error and the bank will still have no accounts.
+	'''
+	def testRemoveNoAccounts(self):
+		#ensure account list is empty
+		self.assertEqual(len(self.b.bankAccounts),0)
+		self.b.removeAccount(1)
+		#ensure account list is still empty
+		self.assertEqual(len(self.b.bankAccounts),0)
+
+
+	'''
+	USES MOCK
+	Test removeAccount to ensure that when the bank has 1 account, after removing
+	the 1 account, the bank will have zero accounts. Uses mock to return the correct
+	account number
+	'''
+	def testRemoveOneAccount(self):
+		with patch('BankApplication.BankAccount') as mock:
+			instance = mock.return_value
+			instance.getAccountNumber.return_value = 1
+			self.b.addAccount()
+
+			#ensure account list's size is 1
+			self.assertEqual(len(self.b.bankAccounts),1)
+
+			#remove account
+			self.b.removeAccount(1)
+
+			#ensure account list's size is 0
+			self.assertEqual(len(self.b.bankAccounts),0)
 
 
 
 	'''
 	USES MOCK
-	Test to ensure that getAccount can be duplicated. Get account, deposit money into it, set account = null,
-	get account again, and make sure the balance is the amount deposited.
+	Test to ensure that getAccount can be duplicated. Get account, deposit money into it, set account = None,
+	get account again, and make sure the balance is the amount deposited
 	'''
+	def testGetAccountTwice(self):
+		with patch('BankApplication.BankAccount') as mock:
+			instance = mock.return_value
+			instance.getAccountNumber.return_value = 1
+			self.b.addAccount()
+			a = self.b.getAccount(1)
+			a.balance = 10
+			a = None
 
+			#Assert that a has been set to None
+			self.assertIsNone(a)
 
-
-
-
-
-
+			#Get the same account and assert the balance is still 10
+			a = self.b.getAccount(1)
+			self.assertEqual(a.balance,10)
 
 
 	'''
 	USES MOCK
-	Tests to ensure that getNextAccountNumber method is called from within addAccount
-
+	Tests to ensure that when addAccount is called,
+	getNextAccountNumber method is called from within addAccount
 	'''
 	def testAddAccountUsingMock(self):
 		real = Bank()
@@ -130,6 +171,41 @@ class testBank(unittest.TestCase):
 		self.b.addAccount()
 		self.b.addAccount()
 		self.assertEqual(self.b.getNextAccountNumber(),3)
+
+
+	'''
+	Test to ensure that calling empytAllAccounts with only accounts with zero balances will have
+	the accounts still have zero balances
+	'''
+	def testEmptyAllAccountsZeroBalance(self):
+		self.b.addAccount()
+		self.b.addAccount()
+		self.b.addAccount()
+		for i in self.b.bankAccounts:
+			self.assertEqual(i.balance,0)
+
+
+	'''
+	Test to ensure that calling emptyAllAccounts with accounts that have balances, the ending balances
+	will all be zero
+	'''
+	def testEmptyAllAccountsNonZeroBalances(self):
+		self.b.addAccount()
+		self.b.addAccount()
+		self.b.addAccount()
+
+		for i in self.b.bankAccounts:
+			i.balance += 10
+
+		#Ensure balances are all 10
+		for i in self.b.bankAccounts:
+			self.assertEqual(i.balance,10)
+
+		self.b.emptyAllAccounts()
+
+		#Ensure all balances are 0
+		for i in self.b.bankAccounts:
+			self.assertEqual(i.balance,0)
 	
 	
 	'''
